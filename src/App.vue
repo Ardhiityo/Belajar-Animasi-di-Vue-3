961ddcd722fd4926e2fa58fee59037806c7f341d
belajar-vue-2 <aryaadi229@gmail.com>
2024-07-02 21:49:59 +0700

    Mengenal Transition Component Dan Menggunakannya

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <transition name="fade">
+      <div v-if="showContent">
+        <h1>Hello World</h1>
+      </div>
+    </transition>
+    <button @click="showContent = !showContent">Click me!</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const showContent = ref(true);
+</script>
+
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter-active {
+  transition: all 1s ease;
+}
+</style>
